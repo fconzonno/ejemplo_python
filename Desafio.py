b38aace5b284c3e046a6a213d441cbc7c027e8db
fconzonno <62101520+fconzonno@users.noreply.github.com>
2021-04-05 13:06:40 -0300

    Update Desafio.py

--- Desafio.py
+++ Desafio.py
@@ -5,5 +5,6 @@ textoCodificado = text.join(list(map(lambda a: chr(ord(a) + 1), texto )))
 
 print (textoCodificado)
 
-
+#Lo que se realizó fue una lista de caracteres incrementados en uno del string ingresado. Luego, cada elemento de la lista se concatena en un string. 
+#Quería saber si hay una manera mas facil de realizarlo, sin necesidad de trabajar con una lista.
 
